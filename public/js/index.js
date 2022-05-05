e4a927767e8717e6a69123fd35475aec6c559183
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-05-05 13:13:12 -0700

    Made colourChooser function

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,154 @@
+const { type } = require("express/lib/response")
+
 to_add = ''
+
+function colourChooser (data) {
+    type = []
+    firstColour = ""
+    secondColour = ""
+    for (k = 0; k < data.types.length; k++) {
+        type += data.types[k].type.name
+    }
+    
+    for (k = 0; k < type.length; k++) {
+        if (type[k] == "normal") {
+            if (k == 0) {
+                firstColour = "wheat"
+            }
+            if (k == 1) {
+                secondColour = "wheat"
+            }
+        }
+        if (type[k] == "fighting") {
+            if (k == 0) {
+                firstColour = "maroon"
+            }
+            if (k == 1) {
+                secondColour = "maroon"
+            }
+        }
+        if (type[k] = "flying") {
+            if (k == 0) {
+                firstColour = "lightskyblue"
+            }
+            if (k == 1) {
+                secondColour = "lightskyblue"
+            }
+        }
+        if (type[k] = "poison") {
+            if (k == 0) {
+                firstColour = "darkviolet"
+            }
+            if (k == 1) {
+                secondColour = "darkviolet"
+            }
+        }
+        if (type[k] = "ground") {
+            if (k == 0) {
+                firstColour = "sandybrown"
+            }
+            if (k == 1) {
+                secondColour = "sandybrown"
+            }
+        }
+        if (type[k] = "rock") {
+            if (k == 0) {
+                firstColour = "darkgoldenrod"
+            }
+            if (k == 1) {
+                secondColour = "darkgoldenrod"
+            }
+        }
+        if (type[k] = "bug") {
+            if (k == 0) {
+                firstColour = "olive"
+            }
+            if (k == 1) {
+                secondColour = "olive"
+            }
+        }
+        if (type[k] = "ghost") {
+            if (k == 0) {
+                firstColour = "rebeccapurple"
+            }
+            if (k == 1) {
+                secondColour = "rebeccapurple"
+            }
+        }
+        if (type[k] = "steel") {
+            if (k == 0) {
+                firstColour = "dimgrey"
+            }
+            if (k == 1) {
+                secondColour = "dimgrey"
+            }
+        }
+        if (type[k] = "fire") {
+            if (k == 0) {
+                firstColour = "orange"
+            }
+            if (k == 1) {
+                secondColour = "orange"
+            }
+        }
+        if (type[k] = "water") {
+            if (k == 0) {
+                firstColour = "dodgerblue"
+            }
+            if (k == 1) {
+                secondColour = "dodgerblue"
+            }
+        }
+        if (type[k] = "grass") {
+            if (k == 0) {
+                firstColour = "forestgreen"
+            }
+            if (k == 1) {
+                secondColour = "forestgreen"
+            }
+        }
+        if (type[k] = "electric") {
+            if (k == 0) {
+                firstColour = "yellow"
+            }
+            if (k == 1) {
+                secondColour = "yellow"
+            }
+        }
+        if (type[k] = "psychic") {
+            if (k == 0) {
+                firstColour = "orchid"
+            }
+            if (k == 1) {
+                secondColour = "orchid"
+            }
+        }
+        if (type[k] = "ice") {
+            if (k == 0) {
+                firstColour = "cyan"
+            }
+            if (k == 1) {
+                secondColour = "cyan"
+            }
+        }
+        if (type[k] = "dragon") {
+            if (k == 0) {
+                firstColour = "darkviolet"
+            }
+            if (k == 1) {
+                secondColour = "darkviolet"
+            }
+        }
+        if (type[k] = "fairy") {
+            if (k == 0) {
+                firstColour = "pink"
+            }
+            if (k == 1) {
+                secondColour = "pink"
+            }
+        }
+    }
+}
 
 function finishprocessPokeResp(data) {
     for (j = 0; j < data.genera.length; j++) {
@@ -39,7 +189,11 @@ async function loadRandomPokemon() {
             success: finishprocessPokeResp
         })
 
-
+        await $.ajax({
+            type: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/${x}`,
+            success: colourChooser
+        })
 
         if (i % 3 == 0) {
             to_add += `</div>`
