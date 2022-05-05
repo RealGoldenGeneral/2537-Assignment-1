232788e7e9fd75ca4cbbed3e2793daff31f03a7f
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-05-05 12:22:18 -0700

    Added rendering for profile.ejs

--- server.js
+++ server.js
@@ -11,4 +11,21 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/html/index.html')
 })
 
+app.get('/profile/:id', function (req, res) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${req.params.id}`
+    data = ""
+    https.get(url, function (https_res) {
+        https_res.on("data", function (chunk) {
+            data += chunk
+        })
+
+        https_res.on("end", function() {
+            res.render("profile.ejs", {
+                "id": req.params.id,
+                "name": data.name
+            })
+        })
+    })
+})
+
 app.use(express.static("./public"))
