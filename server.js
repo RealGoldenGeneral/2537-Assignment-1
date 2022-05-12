1314eae0258c1bab7f05b3e75504d9d08914b79c
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-05-12 12:47:51 -0700

    Added mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 app.set('view engine', 'ejs');
 
 app.listen(process.env.PORT || 5000, function (err) {
