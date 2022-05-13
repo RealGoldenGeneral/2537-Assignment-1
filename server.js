796342e436e5b7c0fdf19fd0eb20f4a96458646b
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-05-12 18:50:39 -0700

    Added CRUD ajax requests

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const bodyparser = require("body-parser")
 app.set('view engine', 'ejs');
 
 app.listen(process.env.PORT || 5000, function (err) {
     if (err)
         console.log(err)
 })
+
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/public/html/index.html')
@@ -57,6 +62,61 @@ const eventSchema = new mongoose.Schema({
 });
 const timelineModel = mongoose.model("timeline", eventSchema);
 
+
+app.get('/timeline/getAllEvents', function (req, res) {
+    timelineModel.find({}, function (err, data) {
+        if (err) {
+            console.log("Error: " + err);
+        } else {
+            console.log("Data: " + data);
+        }
+        res.send(data)
+    });
+})
+
+app.put('/timeline/insert', function (req, res) {
+    console.log(req.body)
+    timelineModel.create({
+        'eventDescription': req.body.eventDescription,
+        'time': req.body.time,
+        'hits': req.body.hits
+    }, function (err, data) {
+        if (err) {
+            console.log("Error: " + err)
+        } else {
+            console.log("Data " + data);
+        }
+        res.send("Successfully inserted.");
+    });
+})
+
+app.get('/timeline/delete/:id', function (req, res) {
+    timelineModel.remove ({
+        '_id': req.params.id
+    }, function (err, data) {
+        if (err) {
+            console.log("Error: " + err);
+        } else {
+            console.log("Data: " + data);
+        }
+        res.send("Successfully deleted.")
+    })
+})
+
+app.get('timeline/increaseHits/:id', function (req, res) {
+    timelineModel.updateOne({
+        '_id': req.params.id
+    }, {
+        $inc: {'hits': 1}
+    }, function (err, data) {
+        if (err) {
+            console.log("Error: " + err);
+        } else {
+            console.log("Data: " + data);
+        }
+        res.send("Successfully Updated.")
+    })
+})
 app.get('/search', function (req, res) {
     res.sendFile(__dirname + '/public/html/search.html')
 })
