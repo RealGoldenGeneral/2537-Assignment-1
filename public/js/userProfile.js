912d6e18af3fdbe9b79c34f0385d3ac6a28367af
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-05-22 12:20:37 -0700

    Added functionality

--- public/js/userProfile.js
+++ public/js/userProfile.js
@@ -1,5 +1,50 @@
+function increaseHits() {
+    x = $(this).attr("id")
+    $.ajax({
+        url: `https://polar-refuge-74063.herokuapp.com/timeline/increaseHits/${x}`,
+        type: "get",
+        success: () => {
+            $("main").empty()
+            loadEvents();
+        }
+    })
+}
+
+function deleteElements() {
+    x = $(this).attr("id")
+    $.ajax({
+        url: `https://polar-refuge-74063.herokuapp.com/timeline/delete/${x}`,
+        type: "get",
+        success: function () {
+            $(`#${x}`).remove()
+        }
+    })
+}
+
+function displayEvents(data) {
+    console.log(data)
+    for (i = 0; i < data.length; i++) {
+        $("main").append(`<div id="${data[i]["_id"]}" class="timeBlock">
+                <p> Description: ${data[i].eventDescription} ${data[i].time}</p>
+                <p> User: ${data[i].user} </p>
+                <p> Hits: ${data[i].hits} </p>
+                <button class="like" id="${data[i]["_id"]}"> Accurate </button>
+                <button class="delete" id="${data[i]["_id"]}"> Delete </button>`)
+    }
+}
+
+function createEvents() {
+    $.ajax({
+        type: "get",
+        url: "/timeline/getAllEventsOfUser",
+        success: displayEvents
+    })
+}
+
 function setup() {
-    
+    createEvents();
+    $("body").on("click", ".like", increaseHits)
+    $("body").on("click", ".delete", deleteElements)
 }
 
 $(document).ready(setup)
