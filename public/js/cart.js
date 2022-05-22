ec39c4c6695be5f2c6a1a828c6ddcb72a307a694
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-05-22 16:14:19 -0700

    Added recalculation

--- public/js/cart.js
+++ public/js/cart.js
@@ -25,6 +25,8 @@ function removeCartItems() {
         url: `/cart/delete/${x}`,
         success: () => {
             $(`#${x}`).remove()
+            $("#checkout").remove()
+            calculatePrice()
         }
     })
 }
