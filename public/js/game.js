33b0efbfaa3bb159adbd6cef8a9f2332b57ed2d0
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-05-28 16:23:42 -0700

    Adjusted game size

--- public/js/game.js
+++ public/js/game.js
@@ -5,62 +5,68 @@ firstCardHasBeenFlipped = false
 
 pokemonPairs = {}
 pokemon_array = []
+to_add = ''
+async function createCards(data) {
+    if (data == "incorrect information") {
+        $("main").append("<p>Invalid game size.</p>")
+    } else {
+        boardSize = parseInt(data[0]) * parseInt(data[2])
+        rowSize = parseInt(data[2])
+        for (i = 0; i < boardSize / 2; i++) {
+            x = Math.floor(Math.random() * 897) + 1
+            pokemon_array[i] = x
+            if (i > 1)
+                for (m = 1; m < i - 1; m++) {
+                    while (x == pokemon_array[m]) {
+                        x = Math.floor(Math.random() * 897) + 1
+                        pokemon_array[i] = x
+                    }
+                }
+            pokemonPairs[x] = 0
+        }
+        for (j = 0; j < boardSize; j++) {
+            x = Math.floor(Math.random() * (boardSize / 2))
+            while (pokemonPairs[pokemon_array[x]] == 2) {
+                x = Math.floor(Math.random() * (boardSize / 2))
+            }
+            if (j == 0) {
+                await $("main").html("<div id='game_grid'>")
+            }
+            await $.ajax({
+                type: "get",
+                url: `https://pokeapi.co/api/v2/pokemon/${pokemon_array[x]}`,
+                success: (data) => {
+                    $("#game_grid").append(`<div class="card">
+                    <img id="img${j + 1}" class="front_face" src="${data.sprites.other["official-artwork"].front_default}" alt="">
+                    <img class="back_face" src="../img/back.jpg" alt="">
+                    </div>`)
+                    pokemonPairs[pokemon_array[x]] += 1
+                    $(".card").css("width", `calc(${100 / rowSize}% - 10px)`)
+                    $(".card").css("height", `${100 / rowSize}%`)
+                }
+            })
+            if (j == boardSize - 1) {
+                await $("main").append("</div>")
+            }
+        }
+    }
+}
 
-function createCards() {
+function getGameSize() {
     $.ajax({
         type: "post",
         data: ({
             gameSize: $("#gridSize").val()
         }),
-        url: "/gameSize",
-        success: (data) => {
-            if (data == "incorrect information") {
-                $("main").append("<p>Invalid game size.</p>")
-            } else {
-                boardSize = parseInt(data[0]) * parseInt(data[2])
-                for (i = 0; i < boardSize / 2; i++) {
-                    x = Math.floor(Math.random() * 897) + 1
-                    pokemon_array[i] = x
-                    if (i > 1)
-                        for (m = 1; m < i - 1; m++) {
-                            if (x == pokemon_array[m]) {
-                                x = Math.floor(Math.random() * 897) + 1
-                                pokemon_array[i] = x
-                            }
-                        }
-                    pokemonPairs[x] = 0
-                }
-                for (j = 0; j < boardSize; j++) {
-                    x = Math.floor(Math.random() * 4)
-                    if (pokemonPairs[pokemon_array[x]] == 2) {
-                        x = Math.floor(Math.random() * 4)
-                    }
-                    if (j == 0) {
-                        $("main").html("<div id='game_grid'>")
-                    }
-                    $.ajax({
-                        type: "get",
-                        url: `https://pokeapi.co/api/v2/pokemon/${pokemon_array[x]}`,
-                        success: (data) => {
-                            $("#game_grid").append(`<div class="card">
-                            <img id="img${j + 1}" class="front_face" src="${data.sprites.other["official-artwork"].front_default}" alt="">
-                            <img class="back_face" src="back.jpg" alt="">
-                            </div>`)
-                        }
-                    })
-                    if (j == boardSize - 1) {
-                        $("main").append("</div>")
-                    }
-                }
-            }
-        }
+        url: "/validateBoardSize",
+        success: createCards
     })
 }
 
 function setup() {
-    $("#submit").click(createCards)
-    $(".card").on("click", function () {
-        $(this).toggleClass("flip")
+    $("#submit").click(getGameSize)
+    $("body").on("click", ".card", function () {
+        $(this).attr("id", "flip")
 
         if (!firstCardHasBeenFlipped) {
             // the first card
@@ -76,23 +82,26 @@ function setup() {
             if (
                 $(`#${firstCard.id}`).attr("src")
                 ==
-                $(`#${secondCard.id}`).attr("src")
+                $(`#${secondCard.id}`).attr("src") 
+                &&
+                $(`#${firstCard.id}`).attr("id")
+                !=
+                $(`#${secondCard.id}`).attr("id")
             ) {
                 console.log("a match!");
                 // update the game state
                 // disable clicking events on these cards
-                $(`#${firstCard.id}`).parent().off("click")
-                $(`#${secondCard.id}`).parent().off("click")
+                $("body").off("click", `.${$(`#${firstCard.id}`).parent()[0].class}`)
+                $("body").off("click", `.${$(`#${secondCard.id}`).parent()[0].class}`)
             } else {
                 console.log("not a match");
                 // unflipping
                 setTimeout(() => {
-                    $(`#${firstCard.id}`).parent().removeClass("flip")
-                    $(`#${secondCard.id}`).parent().removeClass("flip")
+                    $(`#${firstCard.id}`).parent().removeAttr("id")
+                    $(`#${secondCard.id}`).parent().removeAttr("id")
+
                 }, 1000)
             }
-            firstCard = undefined
-            secondCard = undefined
         }
     })
 }
