285c6ab4fd5b15c4d53ce3328843e187e10b70fe
RealGoldenGeneral <ncheng25@my.bcit.ca>
2022-05-28 20:18:21 -0700

    Added game over state

--- public/js/game.js
+++ public/js/game.js
@@ -6,47 +6,55 @@ firstCardHasBeenFlipped = false
 pokemonPairs = {}
 pokemon_array = []
 to_add = ''
+game_score = 0
+timer = 100
 async function createCards(data) {
     if (data == "incorrect information") {
         $("main").append("<p>Invalid game size.</p>")
     } else {
         boardSize = parseInt(data[0]) * parseInt(data[2])
-        rowSize = parseInt(data[2])
-        for (i = 0; i < boardSize / 2; i++) {
-            x = Math.floor(Math.random() * 897) + 1
-            pokemon_array[i] = x
-            if (i > 1)
-                for (m = 1; m < i - 1; m++) {
-                    while (x == pokemon_array[m]) {
-                        x = Math.floor(Math.random() * 897) + 1
-                        pokemon_array[i] = x
+        if (boardSize % 2 == 1) {
+            $("main").append("<p>Invalid game size (odd amount of cards).</p>")
+        } else {
+            columnSize = parseInt(data[0])
+            rowSize = parseInt(data[2])
+            timer = 15 * rowSize + (15 * (rowSize - columnSize))
+            for (i = 0; i < boardSize / 2; i++) {
+                x = Math.floor(Math.random() * 897) + 1
+                pokemon_array[i] = x
+                if (i > 1)
+                    for (m = 1; m < i - 1; m++) {
+                        while (x == pokemon_array[m]) {
+                            x = Math.floor(Math.random() * 897) + 1
+                            pokemon_array[i] = x
+                        }
                     }
-                }
-            pokemonPairs[x] = 0
-        }
-        for (j = 0; j < boardSize; j++) {
-            x = Math.floor(Math.random() * (boardSize / 2))
-            while (pokemonPairs[pokemon_array[x]] == 2) {
+                pokemonPairs[x] = 0
+            }
+            for (j = 0; j < boardSize; j++) {
                 x = Math.floor(Math.random() * (boardSize / 2))
-            }
-            if (j == 0) {
-                await $("main").html("<div id='game_grid'>")
-            }
-            await $.ajax({
-                type: "get",
-                url: `https://pokeapi.co/api/v2/pokemon/${pokemon_array[x]}`,
-                success: (data) => {
-                    $("#game_grid").append(`<div class="card">
+                while (pokemonPairs[pokemon_array[x]] == 2) {
+                    x = Math.floor(Math.random() * (boardSize / 2))
+                }
+                if (j == 0) {
+                    await $("main").html(`<div><h3>Time Remaining: ${timer}</h3></div><div id='game_grid'>`)
+                }
+                await $.ajax({
+                    type: "get",
+                    url: `https://pokeapi.co/api/v2/pokemon/${pokemon_array[x]}`,
+                    success: (data) => {
+                        $("#game_grid").append(`<div class="card">
                     <img id="img${j + 1}" class="front_face" src="${data.sprites.other["official-artwork"].front_default}" alt="">
                     <img class="back_face" src="../img/back.jpg" alt="">
                     </div>`)
-                    pokemonPairs[pokemon_array[x]] += 1
-                    $(".card").css("width", `calc(${100 / rowSize}% - 10px)`)
-                    $(".card").css("height", `${100 / rowSize}%`)
+                        pokemonPairs[pokemon_array[x]] += 1
+                        $(".card").css("width", `calc(${100 / rowSize}% - 10px)`)
+                        $(".card").css("height", `${100 / rowSize}%`)
+                    }
+                })
+                if (j == boardSize - 1) {
+                    await $("main").append("</div></div>")
                 }
-            })
-            if (j == boardSize - 1) {
-                await $("main").append("</div>")
             }
         }
     }
@@ -80,19 +88,20 @@ function setup() {
             console.log(firstCard, secondCard);
             // ccheck if we have match!
             if (
-                $(`#${firstCard.id}`).attr("src")
-                ==
-                $(`#${secondCard.id}`).attr("src") 
-                &&
-                $(`#${firstCard.id}`).attr("id")
-                !=
+                $(`#${firstCard.id}`).attr("src") ==
+                $(`#${secondCard.id}`).attr("src") &&
+                $(`#${firstCard.id}`).attr("id") !=
                 $(`#${secondCard.id}`).attr("id")
             ) {
                 console.log("a match!");
                 // update the game state
+                game_score++
                 // disable clicking events on these cards
                 $("body").off("click", `.${$(`#${firstCard.id}`).parent()[0].class}`)
                 $("body").off("click", `.${$(`#${secondCard.id}`).parent()[0].class}`)
+                if (game_score == pokemon_array.length) {
+                    $("main").append("<div><h3>You win!</h3><button id='replay'>Play Again</button></div>")
+                }
             } else {
                 console.log("not a match");
                 // unflipping
